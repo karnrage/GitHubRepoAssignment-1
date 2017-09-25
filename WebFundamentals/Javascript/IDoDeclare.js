b2d9f8ea0674c334c61f65cb7901041963967990
Catalina Ene <ene.catalina@ymail.com>
2017-09-25 13:34:01 -0500

    assignment I do declare

---
+++ WebFundamentals/Javascript/IDoDeclare.js
@@ -0,0 +1,40 @@
+var myName = 'Catalina'
+{
+    console.log(myName);
+}
+var myLast = 'Ene'
+{
+    console.log(myLast);
+}
+var dayofweek = 'Monday'
+{
+    console.log(dayofweek);
+}
+var favoriteNumber = '19'
+{
+    console.log(favoriteNumber);
+}
+var age = '23'
+{
+    console.log(age);
+}
+var date = "09/25/17"
+{
+    console.log(date);
+}
+var yearBorn = '1994'
+{
+    console.log(yearBorn);
+}
+// var age = '23'
+// {
+//     console.log(yearAge);
+// }
+var readingIsFun = true
+var readingIsBoring = false
+{
+    console.log(readingIsFun)
+}
+{
+    console.log(myName,age,date)
+}
