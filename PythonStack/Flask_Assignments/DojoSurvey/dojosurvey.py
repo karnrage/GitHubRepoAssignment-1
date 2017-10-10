1043a8b3b5803a109107b2541481efd33174bc41
Catalina Ene <ene.catalina@ymail.com>
2017-10-09 20:27:46 -0500

    updated dojo survey assignment with validations

--- PythonStack/Flask_Assignments/DojoSurvey/dojosurvey.py
+++ PythonStack/Flask_Assignments/DojoSurvey/dojosurvey.py
@@ -1,21 +1,23 @@
-from flask import Flask, render_template, request, redirect 
 
-app = Flask(__name__)   
-                         
-@app.route('/')         
-def survey():
-  return render_template("survey.html")
+from flask import Flask, render_template, request, redirect, flash
+app = Flask(__name__)
+app.secret_key = 'ThisIsSecret' 
 
-@app.route('/results')
-def results():
-    return render_template("results.html")
+@app.route('/')
+def index():
+  return render_template("index.html")
 
-@app.route('/survey', methods = ['POST'])
+@app.route('/results', methods=['POST'])
 def create_user():
-    name = request.form["name"]
-    location = request.form["location"]
-    language = request.form["language"]
-    comments = request.form["comments"]
-    return render_template("results.html", name = name, location = location, language = language, comments = comments)
-    
-app.run(debug=True)  
+   name = request.form['name']
+   if len(request.form['name']) == 0:
+       flash('Name cannot be empty!')
+       return redirect('/')
+   else:
+        pass
+   location = request.form['location']
+   language = request.form['language']
+   comment = request.form['comment']
+   return render_template('results.html', name = name, location = location, language = language, comment = comment)
+
+app.run(debug=True) 
